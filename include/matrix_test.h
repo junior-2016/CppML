46f426ca5fdd6ae957b46b26e9dd3001000ca0f6
junior <457030053@qq.com>
2019-05-28 22:36:48 +0800

    init project

---
+++ include/matrix_test.h
@@ -0,0 +1,83 @@
+//
+// Created by junior on 19-5-28.
+//
+
+#ifndef CPPML_MATRIX_TEST_H
+#define CPPML_MATRIX_TEST_H
+
+#include <iostream>
+#include <vector>
+#include <memory>
+#include <cassert>
+#include "mkl.h"
+
+namespace CppML {
+    const size_t alignment = 64;
+
+    // 参考: https://www.zhihu.com/question/265023816 中第三个回答
+    void error_handle(size_t m, size_t n) {
+        fprintf(stderr, "\n ERROR: Can't allocate memory for matrices(%zu x %zu). Aborting... \n", m, n);
+        fflush(stderr);
+        abort(); // 出现malloc错误直接崩溃,不做异常处理,打印错误信息.
+    }
+
+    template<typename DataType, size_t m, size_t n>
+    class Matrix {
+        friend class Algorithm;
+
+    private:
+        DataType *data;
+
+    private:
+        // 下面这个接口过于危险,所以对其private,仅在内部使用(按照自己的内存分配处理)
+        explicit Matrix(DataType *data) : data(data) {
+            static_assert(m >= 1 && n >= 1);
+        }
+
+    public:
+        Matrix() {
+            static_assert(m >= 1 && n >= 1);
+            data = reinterpret_cast<DataType *>(mkl_malloc(m * n * sizeof(DataType), alignment));
+            if (data == nullptr) {
+                error_handle(m, n);
+            }
+        }
+
+        // TODO: 添加边界检查(在debug模式下)
+        // 这里不做空检查,因为data必定非空
+        void set(size_t i, size_t j, const DataType &d) {
+            data[j + i * n] = d;
+        }
+
+        // TODO: 添加边界检查(在debug模式下)
+        // 这里不做空检查,因为data必定非空
+        DataType at(size_t i, size_t j) {
+            return data[j + i * n];
+        }
+
+        ~Matrix() {
+            if (data != nullptr) {
+                mkl_free(data);
+                data = nullptr;
+            }
+        }
+    };
+
+    class Algorithm {
+    public:
+        template<typename DataType__, size_t m_, size_t k_, size_t n_>
+        static Matrix<DataType__, m_, n_> mm(const Matrix<DataType__, m_, k_> &matrix_a,
+                                             const Matrix<DataType__, k_, n_> &matrix_b,
+                                             double alpha = 1.0, double beta = 0.0) {
+            auto result = reinterpret_cast<DataType__ *>(mkl_malloc(m_ * n_ * sizeof(DataType__), alignment));
+            if (result == nullptr) {
+                error_handle(m_, n_);
+            }
+            cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans,
+                        m_, n_, k_, alpha, matrix_a.data, k_, matrix_b.data, n_, beta, result, n_);
+            return Matrix<DataType__, m_, n_>(result);
+        }
+    };
+
+}
+#endif //CPPML_MATRIX_TEST_H
---
+++ src/matrix_test.cpp
@@ -0,0 +1,66 @@
+#include "../include/matrix_test.h"
+
+int main() {
+    // std::cout << sizeof(MKL_INT) << "\n";
+
+    int i, j, data;
+
+    printf("\n This example computes real matrix C=alpha*A*B+beta*C using \n"
+           " Intel(R) MKL function dgemm, where A, B, and  C are matrices and \n"
+           " alpha and beta are double precision scalars\n\n");
+
+    constexpr int m = 5000, k = 5000, n = 5000;
+
+    printf(" Initializing data for matrix multiplication C=A*B for matrix \n"
+           " A(%ix%i) and matrix B(%ix%i)\n\n", m, k, k, n);
+
+    printf(" Intializing matrix data \n\n");
+
+    using namespace CppML;
+    Matrix<double, m, k> A;
+    Matrix<double, k, n> B;
+    data = 0;
+    for (i = 0; i < m; i++) {
+        for (j = 0; j < k; j++) {
+            A.set(i, j, (data++ + 1));
+        }
+    }
+    data = 0;
+    for (i = 0; i < k; i++) {
+        for (j = 0; j < n; j++) {
+            B.set(i, j, (-data++ - 1));
+        }
+    }
+
+    printf(" Computing matrix product using Intel(R) MKL dgemm function via CBLAS interface \n\n");
+    // m x k  and k x n
+    auto C = Algorithm::mm(A, B);
+    printf("\n Computations completed.\n\n");
+
+    printf(" Top left corner of matrix A: \n");
+    for (i = 0; i < std::min(m, 6); i++) {
+        for (j = 0; j < std::min(k, 6); j++) {
+            printf("%12.0f", A.at(i, j));
+        }
+        printf("\n");
+    }
+
+    printf("\n Top left corner of matrix B: \n");
+    for (i = 0; i < std::min(k, 6); i++) {
+        for (j = 0; j < std::min(n, 6); j++) {
+            printf("%12.0f", B.at(i, j));
+        }
+        printf("\n");
+    }
+
+    printf("\n Top left corner of matrix C: \n");
+    for (i = 0; i < std::min(m, 6); i++) {
+        for (j = 0; j < std::min(n, 6); j++) {
+            printf("%12.5G", C.at(i, j));
+        }
+        printf("\n");
+    }
+
+    printf(" Example completed. \n\n");
+    return 0;
+}
